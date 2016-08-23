17aefc4ae8f090472e5e2aec274239765f34c476
SuperApollo <wpb504@126.com>
2016-08-23 16:17:05 +0800

    create
    first time to create project

---
+++ BLEChat/app/src/main/java/com/example/administrator/apolloblechat/activity/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.administrator.apolloblechat.activity;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Administrator on 2016/8/23.
+ */
+public class BaseActivity extends FragmentActivity {
+
+
+}
---
+++ BLEChat/app/src/main/java/com/example/administrator/apolloblechat/fragment/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.example.administrator.apolloblechat.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by Administrator on 2016/8/23.
+ */
+public class BaseFragment extends Fragment {
+}
