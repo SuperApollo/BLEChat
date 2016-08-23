47d42a961c3f355d81669f2f8fbaa5da759e9f92
SuperApollo <wpb504@126.com>
2016-08-23 17:24:21 +0800

    提交

--- BLEChat/app/src/main/java/com/example/administrator/apolloblechat/activity/BaseActivity.java
+++ BLEChat/app/src/main/java/com/example/administrator/apolloblechat/activity/BaseActivity.java
@@ -1,11 +1,21 @@
 package com.example.administrator.apolloblechat.activity;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+
+import com.example.administrator.apolloblechat.R;
 
 /**
  * Created by Administrator on 2016/8/23.
  */
 public class BaseActivity extends FragmentActivity {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.);
+    }
 
 }
