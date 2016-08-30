855a4fae4ec27d4c03cd91bf2ac8cf19951c822d
SuperApollo <wpb504@126.com>
2016-08-30 11:30:21 +0800

    增加聊天和传文件fragment

---
+++ BLEChat/app/src/main/java/com/example/administrator/apolloblechat/fragment/ChatFragment.java
@@ -0,0 +1,13 @@
+package com.example.administrator.apolloblechat.fragment;
+
+import com.example.administrator.apolloblechat.R;
+
+/**
+ * Created by Administrator on 2016/8/30.
+ */
+public class ChatFragment extends BaseFragment {
+    @Override
+    protected int getViewId() {
+        return R.layout.fragment_chat;
+    }
+}
