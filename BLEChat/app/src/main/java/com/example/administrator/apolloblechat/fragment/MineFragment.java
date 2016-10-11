70163e10c45c092bba974586565168a24a987eaf
SuperApollo <wpb504@126.com>
2016-10-11 10:38:29 +0800

    注册完善

--- BLEChat/app/src/main/java/com/example/administrator/apolloblechat/fragment/MineFragment.java
+++ BLEChat/app/src/main/java/com/example/administrator/apolloblechat/fragment/MineFragment.java
@@ -3,12 +3,17 @@ package com.example.administrator.apolloblechat.fragment;
 import android.view.View;
 
 import com.example.administrator.apolloblechat.R;
+import com.example.administrator.apolloblechat.utils.FragmentUtils;
+import com.example.administrator.apolloblechat.widgets.MyTittleBar;
 
 /**
  * Created by Administrator on 2016/9/23.
  */
 
 public class MineFragment extends BaseFragment {
+
+    private MyTittleBar title_mine;
+
     @Override
     protected int getViewId() {
         return R.layout.fragment_mine;
@@ -16,6 +21,12 @@ public class MineFragment extends BaseFragment {
 
     @Override
     protected void initView(View view) {
-
+        title_mine = queryViewById(view, R.id.title_mine);
+        title_mine.setOnBackListener(new MyTittleBar.OnBackListener() {
+            @Override
+            public void onBackClick() {
+                FragmentUtils.replace(getActivity(),R.id.ll_fragment_container,new MoreFragment());
+            }
+        });
     }
 }
